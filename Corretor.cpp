0d14c90ae7bf0a09a2c9321c83b9accea4953810
Pietro <pietroz@terra.com.br>
2018-07-31 19:20:32 -0300

    Função Corretor::ImprimeContexto

--- Corretor.cpp
+++ Corretor.cpp
@@ -1,11 +1,16 @@
 #include "Corretor.h"
 
-void ImprimeContexto(list<Palavra>::iterator Atual) {
+void ImprimeContexto(list<Palavra>::iterator Inicio, list<Palavra>::iterator Atual, list<Palavra>::iterator Fim) {
 
     list<Palavra>::iterator Anterior = prev(Atual, 1);       
     list<Palavra>::iterator Proximo = next(Atual, 1);
 
-
+    if( Atual == Inicio )
+        cout << "NULL" << " - " << *Atual << " - " << *Proximo << endl;
+    else if (Atual == Fim )
+        cout << *Anterior << " - " << *Atual << " - " << "NULL" << endl;
+    else
+        cout << *Anterior << " - " << *Atual << " - " << *Proximo << endl;    
 
 
 }
@@ -26,13 +31,7 @@ void Corretor::Compara(Texto &T, Dicionario &D) {
             list<Palavra>::iterator Anterior = prev(it, 1);       
             list<Palavra>::iterator Proximo = next(it, 1);
 
-            if( *it == TextWords.begin())
-                cout << "NULL" << " - " << *it << " - " << *Proximo << endl;
-            else if (*it == TextWords.end())
-                cout << *Anterior << " - " << *it << " - " << "NULL" << endl;
-            else
-                cout << *Anterior << " - " << *it << " - " << *Proximo << endl;          
-
+            ImprimeContexto(TextWords.begin(), it, TextWords.end());         
             
 
             // Coloca a palavra errada na lista de Erros do Corretor
