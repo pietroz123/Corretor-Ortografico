35291e5510e04ff48da5c8a2c883a3ff664e364d
Pietro <pietroz@terra.com.br>
2018-08-09 23:29:59 -0300

    mainTexto

--- mainTexto.cpp
+++ mainTexto.cpp
@@ -6,23 +6,47 @@ using namespace std;
 int main() {
 
     Texto T;
-    string Nome_Arquivo;
 
-    cout << "Digite o nome do arquivo que quer abrir: ";
-    getline(cin, Nome_Arquivo);
-
-    T.setNomeArquivo(Nome_Arquivo);
     T.CarregarTexto();
 
-    cout << "Texto do arquivo:" << endl;
-    T.ImprimirTexto();
+    int Opcao = -1;
+    while (Opcao != 0) {
+
+        cout << "(0) Sair (1) Imprimir (2) Buscar" << endl;
+        cout << "Opcao: ";
+        cin >> Opcao;
+
+        switch(Opcao) {
+
+            case 1: {
+                T.ImprimirTexto();
+                break;
+            }
+            case 2: {
+
+                Palavra P2;
+                cout << endl << "Digite uma palavra para busca: ";
+                cin >> P2;
+                
+                if (T.Buscar(P2))
+                    cout << "A palavra '" << P2 << "' pertence ao texto!" << endl << endl;
+                else
+                    cout << "A palavra '" << P2 << "' NAO pertence ao texto!" << endl << endl;
+
+                break;
+            }
+
+        }
+
+    }
 
     cout << endl << "Deseja gravar o texto em outro arquivo? (1) SIM (2) NAO: ";
-    int Opcao;
-    cin >> Opcao;
-    if (Opcao == 1) {
-        T.GravarTexto();
+    int Resposta;
+    cin >> Resposta;
+    if (Resposta == 1) {
+        T.GravarTextoDiferente();
     }
+
 
 
     return 0;
