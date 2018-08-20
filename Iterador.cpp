880c96367703853a985339b50ad0bdb2650bc8b5
Pietro <pietroz@terra.com.br>
2018-08-19 23:27:06 -0300

    Criação da classe Iterador

---
+++ Iterador.cpp
@@ -0,0 +1,31 @@
+#include "Iterador.h"
+
+Iterador::Iterador(No *raiz) : Atual(raiz) { }
+
+bool Iterador::operator==(const Iterador &it) {
+    
+}
+
+bool Iterador::operator!=(const Iterador &Outro) {
+
+}
+
+Palavra &Iterador::operator*() const {
+
+}
+
+Iterador Iterador::operator++() {
+
+}
+
+Iterador Iterador::operator++(int n) {
+
+}
+
+Iterador Iterador::operator--() {
+
+}
+
+Iterador Iterador::operator--(int n) {
+
+}
--- Iterador.h
+++ Iterador.h
@@ -9,15 +9,17 @@ class Iterador {
 
     private:
         No *Atual;
+        No *SucessorEmOrdem(No *N);
 
     public:
-        Iterador(No *Raiz) {
-            this->Atual = Raiz;
-        }
-        Iterador Proximo(Iterador it) {
-            it.Atual = sucessor_em_ordem(it);
-            return it;
-        }
+        Iterador(No *raiz);
+        bool operator==(const Iterador &it);        // comparação entre iteradores
+        bool operator!=(const Iterador &Outro);      
+        Palavra &operator*() const;                 // acessa e retorna a Palavra
+        Iterador operator++();                      // incrementa para o próximo valor maior
+        Iterador operator++(int n);                 // incrementa em n posições
+        Iterador operator--();                      // decrementa para o valor anterior
+        Iterador operator--(int n);                 // decrementa em n posições
 
 };
 
